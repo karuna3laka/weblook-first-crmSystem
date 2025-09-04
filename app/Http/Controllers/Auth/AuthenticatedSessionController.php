9418bc1a9a2324a876517c464a07922615c46f82
Kavindu Vidushan Karunathilaka <143307455+karuna3laka@users.noreply.github.com>
2025-09-04 11:10:41 +0530

    Final check minor changes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -12,17 +12,13 @@ use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
+    //display
     public function create(): View
     {
         return view('auth.login');
     }
 
-    /**
-     * Handle an incoming authentication request.
-     */
+    //incoming reqs
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
@@ -32,9 +28,7 @@ class AuthenticatedSessionController extends Controller
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
+    //delete auth session
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
